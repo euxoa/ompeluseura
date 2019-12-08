254777853d48d30a659ba9ce34795642ccd7e4c7
Janne Sinkkonen <janne.sinkkonen@gmail.com>
2019-12-08 22:58:29 +0200

    state-space model

--- snow.R
+++ snow.R
@@ -41,3 +41,43 @@ fit
 hist(posterior_samples(fit, "k")$k, n=100)
 plot(fit, pars=c("k", "b"))
 plot(fit, pars="prob")
+
+# Vectorized state-space model
+
+model_str2 <- "
+data {
+   int N;
+   real<lower=0> snow[N]; }
+transformed data {
+   int<lower=0, upper=1> snowp[N];
+   for (i in 1:N) snowp[i] = (snow[i] > 0); }
+parameters {
+   vector[N] innovations;
+   real base;
+   real<lower=0> sigma;
+   real ytrend;
+   }
+model {
+   innovations ~ normal(ytrend, 1);
+   snowp ~ bernoulli_logit(base + sigma * cumulative_sum(innovations));
+   base ~ normal(0, 5);
+   sigma ~ normal(0, .1);
+   ytrend ~ normal(0, .1);
+}
+generated quantities {
+   vector[N] prob;
+   prob = inv_logit(base + sigma * cumulative_sum(innovations));
+}
+"
+m2 <- stan_model(model_code = model_str2)
+fit2 <- sampling(m2, data=stan_data, control=list(adapt_delta=.95))
+plot(fit2, par="prob")
+
+christmas %>% filter(!is.na(snow)) %>% 
+  mutate(p=apply(extract(fit2, "prob")[[1]], 2, mean)) %>% 
+  ggplot(aes(x=year, y=p)) + geom_line() 
+
+plot(fit2, pars=c("ytrend", "sigma"))
+change_samples <- extract(fit2, "prob[1]")[[1]] - extract(fit2, "prob[60]")[[1]]
+hist(change_samples, n=100)
+mean(change_samples<0)
